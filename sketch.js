7b7ec4992eef887ca918143d8a51832220caffee
nhsegal <n.h.segal@gmail.com>
2021-01-22 16:53:38 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,8 +47,9 @@ function draw() {
   background(237);
   strokeWeight(.2);
   for (let i = 0; i < 23; i++){
-    line(0,40*i,800,40*i);
-    line(40*i,0, 40*i, 500);
+    console.log(i);
+    line(0,40*i,width,40*i);
+    line(40*i,0, 40*i, height);
   }
   ball.update();
   ball.checkEdges();
